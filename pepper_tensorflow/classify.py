181f57b5e28d70dd8544897d1b3a6e035b32d8d0
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2017-10-16 10:37:13 +0200

    update ClassifyClient and add documentation

--- pepper_tensorflow/classify.py
+++ pepper_tensorflow/classify.py
@@ -72,7 +72,16 @@ class NodeLookup:
 class Classify:
     IMAGE_GRAPH = os.path.join(MODEL_DIR, r'classify_image_graph_def.pb')
 
-    def __init__(self, n_predictions: int = 5):
+    def __init__(self, n_predictions: int = 1):
+        """
+        Classify Jpeg images
+
+        Parameters
+        ----------
+        n_predictions: int
+            Number of predictions to make per image
+        """
+
         with tf.gfile.FastGFile(self.IMAGE_GRAPH, 'rb') as graph_file:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(graph_file.read())
@@ -84,6 +93,19 @@ class Classify:
         self.softmax = self.session.graph.get_tensor_by_name('softmax:0')
 
     def classify(self, jpeg: bytes):
+        """
+        Classify Jpeg
+
+        Parameters
+        ----------
+        jpeg: bytes
+            Jpeg image to classify
+
+        Returns
+        -------
+        classification: list of (float, list)
+            List of confidence-object pairs
+        """
         with tf.device('/cpu:0'):
             predictions = np.squeeze(self.session.run(self.softmax, {'DecodeJpeg/contents:0': jpeg}))
 
@@ -95,49 +117,51 @@ class Classify:
         self.session.close()
 
 
-class ClassifyRequestHandlerOLD(BaseRequestHandler):
-
-    CLASSIFY = Classify()
-
-    def handle(self):
-        try:
-            path = self.request.recv(4096).decode()
-
-            t0 = time()
-            classification = self.CLASSIFY.classify(path)
-            print("[{}][{:3.2f}s] {}: [{:3.0%}] {}".format(
-                strftime("%H:%M:%S"), time() - t0, self.client_address,
-                classification[0][0], classification[0][1]))
-
-            self.request.sendall(yaml.dump(classification).encode())
-
-        except ConnectionResetError:
-            pass
-
-
 class ClassifyRequestHandler(BaseRequestHandler):
 
     CLASSIFY = Classify()
 
     def handle(self):
+        """
+        Handle client classification request
+        """
         try:
 
             t0 = time()
-            jpeg_size = int(np.frombuffer(self.request.recv(4), np.uint32)[0])
-            jpeg = self._receive_bytes(jpeg_size)
 
+            # Receive Jpeg
+            jpeg_size = int(np.frombuffer(self.request.recv(4), np.uint32)[0])  # Jpeg size is transmitted as uint32
+            jpeg = self._receive_bytes(jpeg_size)  # Jpeg is transmitted as 'jpeg_size' bytes
+
+            # Classify Jpeg
             classification = self.CLASSIFY.classify(jpeg)
 
+            # Return yaml-encoded classification
+            self.request.sendall(yaml.dump(classification).encode())
+
+            # Print statistics
             print("[{}][{:3.2f}s] {}: [{:3.0%}] {}".format(
                 strftime("%H:%M:%S"), time() - t0, self.client_address,
                 classification[0][0], classification[0][1]))
 
-            self.request.sendall(yaml.dump(classification).encode())
-
+        # If the client disconnects, that is ok
         except ConnectionResetError:
             pass
 
-    def _receive_bytes(self, n):
+    def _receive_bytes(self, n: int):
+        """
+        Receive exactly n bytes
+
+        Parameters
+        ----------
+        n: int
+            Number of bytes to receive
+
+        Returns
+        -------
+        bytes
+        """
+
         jpeg_buffer = bytearray()
 
         while len(jpeg_buffer) < n:
@@ -148,19 +172,57 @@ class ClassifyRequestHandler(BaseRequestHandler):
 
 class ClassifyServer(TCPServer):
     def __init__(self, port: int, deamon: bool = False):
+        """
+        Run Classify Server
+
+        Parameters
+        ----------
+        port: int
+            Port to listen to for incoming classification requests
+        deamon: bool
+            Whether server thread should be daemon or not
+        """
         super().__init__(('', port), ClassifyRequestHandler)
         Thread(target=self.serve_forever, daemon=deamon).start()
 
 
 class ClassifyClient:
     def __init__(self, address: tuple):
+        """
+        Sample Classification Client
+
+        Parameters
+        ----------
+        address: (str, int)
+            Address of Classification Host
+        """
+
         self.address = address
 
     def classify(self, path: str):
+        """
+        Classify Jpeg Image on disk
+
+        Parameters
+        ----------
+        path: str
+            Path to Jpeg image on disk
+
+        Returns
+        -------
+        classification: list of (float, list)
+            List of confidence-object pairs
+        """
         sock = socket()
 
         sock.connect(self.address)
-        sock.sendall(path.encode())
+
+        with open(path, 'rb') as jpeg_file:
+            jpeg = jpeg_file.read()
+            jpeg_size = np.array([len(jpeg)], np.uint32)
+
+        sock.sendall(jpeg_size)
+        sock.sendall(jpeg)
 
         response = yaml.load(sock.recv(4096).decode())
 
