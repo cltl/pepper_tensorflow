21d274b47ce739b3d55c5a496c340a5dc1092510
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2018-02-12 12:38:14 +0100

    add gender classification data  + less biased pretrained model (93.68% accuracy) this model was trained on a subset of the "IMDB" dataset and validated on the "WIKI" dataset.

--- pepper_tensorflow/gender.py
+++ pepper_tensorflow/gender.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from socketserver import TCPServer, BaseRequestHandler
+import socket
 from threading import Thread
 import os
 
@@ -20,7 +21,10 @@ class GenderModel:
         self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)  # Softmax Function
         self.y_ = tf.placeholder(tf.float32, [None, 2])             # Correct Answers
 
-    def train(self, face: np.ndarray, gender: np.ndarray, learning_rate = 0.5, training_steps = 2000):
+        
+
+    def train(self, face: np.ndarray, gender: np.ndarray,
+              learning_rate = 0.25, training_steps = 5000, batch_size = 2000):
         """
         Train Model with Training Data, Saving Results to File
 
@@ -34,6 +38,8 @@ class GenderModel:
             Model Learning Rate
         training_steps: int
             Model Training Steps
+        batch_size: int
+            Model Batch Size
         """
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
@@ -43,7 +49,8 @@ class GenderModel:
             print("Train Model, with {} data entries".format(len(face)))
 
             for i in range(training_steps):
-                train_step.run(feed_dict={self.x: face, self.y_: gender})
+                batch_indices = np.random.choice(np.arange(len(face)), batch_size)
+                train_step.run(feed_dict={self.x: face[batch_indices], self.y_: gender[batch_indices]})
                 print("\rTraining Step {}/{}".format(i + 1, training_steps), end='')
             tf.train.Saver().save(session, self.MODEL_PATH)
             print("\rSaved model in {}".format(os.path.abspath(self.MODEL_PATH)))
@@ -86,6 +93,7 @@ class GenderModel:
         face = np.fromfile(FACE_PATH, np.float32).reshape(-1, 128)
         gender_age = np.fromfile(GENDER_PATH, np.byte).view([('gender',np.bool), ('age',np.float32)]).view(np.recarray)
         gender = np.eye(2)[gender_age['gender'].astype(np.int32)]  # One Hot Encoding of Gender
+        print(np.mean(gender, 0))
         return face, gender
 
 
@@ -118,13 +126,10 @@ class GenderClassify(GenderModel):
 class ClassifyRequestHandler(BaseRequestHandler):
     """Handle Client Gender Classification Requests"""
 
-    CLASSIFY = GenderClassify()
     BUFFER_SIZE = 4096
 
     def handle(self):
         """Handle Classification Request"""
-
-        print("Got Request")
 
         # Receive int32 indicating how many bytes will be sent
         n_bytes = np.frombuffer(self.request.recv(4), np.int32)
@@ -135,7 +140,7 @@ class ClassifyRequestHandler(BaseRequestHandler):
             face_buffer.extend(self.request.recv(self.BUFFER_SIZE))
         face = np.frombuffer(face_buffer, np.float32).reshape(-1, 128)
 
-        classification = self.CLASSIFY.classify(face)
+        classification = self.server.classify.classify(face)
 
         # Send Number of Bytes to be sent Back
         self.request.sendall(np.int32(len(classification)))
@@ -158,10 +163,63 @@ class ClassifyServer(TCPServer):
         deamon: bool
             Whether server thread should be daemon or not
         """
+
+        self.classify = GenderClassify()
+
         super().__init__(('', port), ClassifyRequestHandler)
         Thread(target=self.serve_forever, daemon=daemon).start()
         print("Gender Classification Server Booted")
 
 
+class ClassifyClient(object):
+
+    BUFFER_SIZE = 4096
+
+    def __init__(self, address = ('localhost', 8678)):
+        """
+        Classify Gender From OpenFace Face Representations
+
+        Requires Running the GenderClassifyServer in the pepper_tensorflow repository
+
+        Parameters
+        ----------
+        address: (str, int)
+            Tuple of (<host>, <port>), where server is located
+        """
+        self.address = address
+
+    def classify(self, face):
+        """
+        Classify Face(s)
+
+        Parameters
+        ----------
+        face: np.ndarray
+            OpenFace 128D Face Vector(s)
+
+        Returns
+        -------
+        classification: np.ndarray
+            One float per face, indicating P("Female") == 1 - P("Male")
+        """
+        s = socket.socket()
+        s.connect(self.address)
+        s.sendall(np.int32(face.nbytes))
+        s.send(face)
+
+        response_length = np.frombuffer(s.recv(4), np.int32)
+        response_buffer = bytearray()
+        while len(response_buffer) < response_length:
+            response_buffer.extend(s.recv(self.BUFFER_SIZE))
+        return np.frombuffer(response_buffer, np.float32)
+
+
 if __name__ == "__main__":
+
+    # # Train & Test Model
+    # model = GenderModel()
+    # model.train(*GenderModel.load_data('imdb'))
+    # model.test(*GenderModel.load_data('wiki'))
+
+    # Run Server
     ClassifyServer()
